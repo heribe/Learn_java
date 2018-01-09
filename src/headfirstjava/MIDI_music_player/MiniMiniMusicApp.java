d6e95532f27b6a23f61fe3e2db25bee7d90650fa
Maxtrue <zhb384708358@163.com>
2018-01-09 14:33:58 +0800

    MiniMusicCmdLine

--- src/headfirstjava/MIDI_music_player/MiniMiniMusicApp.java
+++ src/headfirstjava/MIDI_music_player/MiniMiniMusicApp.java
@@ -1,6 +1,6 @@
 package headfirstjava.MIDI_music_player;
 /* created at 19:50 2017/11/6  */
-
+/* 声音播放程序 */
 //Sequencer-(palys)-Sequence-(has a)-Track-(holds)-MidiEvent
 
 import javax.sound.midi.*;
@@ -15,22 +15,28 @@ public class MiniMiniMusicApp {
     public void play(){
 
         try {
-            Sequencer player = MidiSystem.getSequencer();
-            player.open();
+            Sequencer player = MidiSystem.getSequencer(); //取得Sequencer
+            player.open(); //打开Sequencer
 
-            Sequence seq = new Sequence(Sequence.PPQ, 4);
+            Sequence seq = new Sequence(Sequence.PPQ, 4); //创建Sequence
 
-            Track track = seq.createTrack();
+            Track track = seq.createTrack(); //要求取得track
+
+            ShortMessage first = new ShortMessage();
+            first.setMessage(192, 1, 5, 100);
+            MidiEvent changeInstrument = new MidiEvent(first, 1);
+            track.add(changeInstrument);
 
             ShortMessage a = new ShortMessage();
-            a.setMessage(144, 1, 70, 100);  //信息类型，频道，音符(0~127)，音量
+            a.setMessage(144, 1, 70, 100);
+                    //信息类型（192表示换乐器，144表示打开，128表示关闭），频道，音符(0~127)，音量
             MidiEvent noteOn = new MidiEvent(a, 1);
-            track.add(noteOn);
-            //message描述做什么,MidiEvent指定何时做
+            track.add(noteOn); //将MidiEvent填入track
+            //message描述做什么,MidiEvent由message和发音时机组成
 
             ShortMessage b = new ShortMessage();
-            b.setMessage(128, 1, 44, 100);
-            MidiEvent noteOff = new MidiEvent(b, 14);
+            b.setMessage(128, 1, 60, 100);
+            MidiEvent noteOff = new MidiEvent(b, 10);
             track.add(noteOff);
 
             player.setSequence(seq);  //将sequence送到sequencer上
--- src/headfirstjava/MIDI_music_player/MusicTest1.java
+++ src/headfirstjava/MIDI_music_player/MusicTest1.java
@@ -1,5 +1,6 @@
 package headfirstjava.MIDI_music_player;
 /* created at 20:07 2017/11/5  */
+/* 判断是否可以取得Sequencer对象 */
 
 import javax.sound.midi.*;
 
@@ -7,15 +8,15 @@ public class MusicTest1 {
 
     public void play() {
         try {
-            Sequencer sequencer = MidiSystem.getSequencer();
+            Sequencer sequencer = MidiSystem.getSequencer(); //这个对象的作用是将MIDI信息组合成“乐曲”
             System.out.println("We got a sequencer");
         }catch(MidiUnavailableException ex) {
             System.out.println("Bummer");
-        }
+        } //关闭播放
     }
 
     public static void main(String[] args) {
         MusicTest1 mt = new MusicTest1();
         mt.play();
-    }
-}
+    } // 关闭main
+} //关闭类
